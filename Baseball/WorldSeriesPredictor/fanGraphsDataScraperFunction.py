63d321c3c006981ac39433b9c1566a1f85b33222
krevice <58454005+krevice@users.noreply.github.com>
2023-08-11 16:24:31 -0700

    Update fanGraphsDataScraperFunction.py

--- Baseball/WorldSeriesPredictor/fanGraphsDataScraperFunction.py
+++ Baseball/WorldSeriesPredictor/fanGraphsDataScraperFunction.py
@@ -4,10 +4,38 @@ import pandas as pd
 import re
 
 def fg_scrape(team, years):
-# Define the dictionary to map team names to team IDs for BASEBALL SAVANT URL
+# Define the dictionary to map team names to team IDs for FANGRAPHS URL
     mlb_teams = {
         'Arizona Diamondbacks': 'diamondbacks',
-        'Atlanta Braves': 'braves'
+        'Atlanta Braves': 'braves',
+        'Baltimore Orioles': 'orioles',
+        'Boston Red Sox': 'red-sox',
+        'Chicago Cubs': 'cubs',
+        'Chicago White Sox': 'white-sox',
+        'Cincinnati Reds': 'reds',
+        'Cleveland Guardians': 'guardians',
+        'Colorado Rockies': 'rockies',
+        'Detroit Tigers': 'tigers',
+        'Houston Astros': 'astros',
+        'Kansas City Royals': 'royals',
+        'Los Angeles Angels': 'angels',
+        'Los Angeles Dodgers': 'dodgers',
+        'Miami Marlins': 'marlins',
+        'Milwaukee Brewers': 'brewers',
+        'Minnesota Twins': 'twins',
+        'New York Mets': 'mets',
+        'New York Yankees': 'yankees',
+        'Oakland Athletics': 'athletics',
+        'Philadelphia Phillies': 'phillies',
+        'Pittsburgh Pirates': 'pirates',
+        'San Diego Padres': 'padres',
+        'San Francisco Giants': 'giants',
+        'Seattle Mariners': 'mariners',
+        'St. Louis Cardinals': 'cardinals',
+        'Tampa Bay Rays': 'rays',
+        'Texas Rangers': 'rangers',
+        'Toronto Blue Jays': 'blue-jays',
+        'Washington Nationals': 'nationals'
     }
 
 ########### Capture Fangraphs tables for MLB team in a season ###########
